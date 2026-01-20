52fc01c325c4256df8dacbccbfb9091f09e5b2fb
viktormoe <viktor@moe.se>
2026-01-20 11:31:55 +0100

    feat: Add `test1.java` demonstrating various `for` and `while` loops for printing number sequences.

---
+++ test1.java
@@ -0,0 +1,59 @@
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println();
+
+        for (int i = 1; i < 9; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 8; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= 8; i++) {
+            System.out.print(i * i + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 1; i <= 11; i += 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 11; i >= -9; i -= 4) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        System.out.println();
+
+        int i = 8;
+        while (i <= 1) {
+            System.out.print(i + " ");
+            i -= 1;
+        }
+
+        System.out.println();
+
+        int ii = 1;
+        while (ii <= 11) {
+            System.out.print(ii + " ");
+            ii += 2;
+        }
+
+        System.out.println();
+
+        int iii = 11;
+        while (iii <= -9) {
+            System.out.print(iii + " ");
+            iii += 4;
+        }
+
+    }
+}
